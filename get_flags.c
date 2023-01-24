182e4999f72ed5c53dce2d34be1bc589ec709067
peppymastermind <koranchieperpetual94@gmail.com>
2023-01-24 07:52:34 -0800

    team project

--- get_flags.c
+++ get_flags.c
@@ -1,42 +1,35 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
+ * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
+ * @i: take a parameter.
+ * Return: Flags:
  */
-int get_precision(const char *format, int *i, va_list list)
+int get_flags(const char *format, int *i)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int j, curr_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	if (format[curr_i] != '.')
-		return (precision);
-
-	precision = 0;
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
+
+		if (FLAGS_CH[j] == 0)
 			break;
 	}
 
 	*i = curr_i - 1;
 
-	return (precision);
+	return (flags);
 }
 
